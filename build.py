582682bf20a5ad398733080792fc7bd150e0f784
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-03-16 03:25:00 +0900

    Specify Dockerfile path, not its directory

--- build.py
+++ build.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
 
 import json
-import os
 import subprocess
 import sys
-from contextlib import contextmanager
 from pathlib import Path
 from typing import Dict, List
 
 
-@contextmanager
-def change_directory(path: Path):
-    original = Path.cwd().resolve()
+def build(dockerfile_path: Path,
+          arguments: Dict[str, str],
+          tags: List[str],
+          options: List[str]):
+    command = ['docker', 'build']
+    command.append(f'--file={dockerfile_path}')
+    command.extend(options)
 
-    os.chdir(path)
-    yield
-    os.chdir(original)
+    for key, value in arguments.items():
+        command.append('--build-arg')
+        command.append(f'{key}={value}')
 
+    command.extend([f'--tag={tag}' for tag in tags])
+    command.append(str(dockerfile_path.parent.resolve()))
 
-def build(path: Path, arguments: Dict[str, str], tags: List[str], options: List[str]):
-    with change_directory(path):
-        command = ['docker', 'build']
-        command += options
-
-        for key, value in arguments.items():
-            command.append('--build-arg')
-            command.append(f'{key}={value}')
-
-        command += [f'--tag={tag}' for tag in tags]
-        command += ['.']
-
-        subprocess.run(command, check=True)
+    subprocess.run(command, check=True)
 
     for tag in tags:
         subprocess.run(['docker', 'push', tag], check=True)
@@ -43,7 +35,7 @@ if __name__ == '__main__':
         config = json.load(input_file)
 
     for item in config['builds']:
-        build(path=Path(item['dockerfile']),
+        build(dockerfile_path=Path(item['dockerfile']),
               arguments=item.get('arguments', {}),
               tags=item['tags'],
               options=build_options)
