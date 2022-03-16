011e40d1274ab83ff0110d4ed57468be0ba5b317
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-03-17 00:51:28 +0900

    Refactor code

--- build.py
+++ build.py
@@ -6,10 +6,10 @@ import sys
 import time
 from collections import OrderedDict
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, Dict, List
 
 
-def retry(number_of_attempts):
+def retry(number_of_attempts: int):
     def decorator(func):
         def wrapper(*args, **kwargs):
             for attempt in range(1, number_of_attempts + 1):
@@ -22,9 +22,42 @@ def retry(number_of_attempts):
                     print('An error occurred:', exception)
                     print('Retry after 15 seconds...')
                     time.sleep(15)
-
         return wrapper
     return decorator
+
+def load_config(filename: str) -> Dict[str, Any]:
+    with open(filename, 'r', encoding='utf-8') as input_file:
+        config = json.load(input_file)
+
+    for build in config['builds']:
+        build['dockerfile_path'] = Path(build['dockerfile'])
+
+        if 'arguments' not in build:
+            build['arguments'] = {}
+
+    return config
+
+def generate_readme(filename: str, config: Dict[str, Any]):
+    def title_of(dockerfile_path: Path) -> str:
+        if dockerfile_path.name == 'Dockerfile':
+            return str(dockerfile_path.parent)
+        elif dockerfile_path.suffix == '.Dockerfile':
+            return str(dockerfile_path.with_suffix(''))
+        else:
+            return str(dockerfile_path)
+
+    tags_by_dockerfile_path = OrderedDict()
+    for item in config['builds']:
+        tags_by_dockerfile_path.setdefault(item['dockerfile_path'], []).extend(item['tags'])
+
+    with open(filename, 'w', encoding='utf-8') as output_file:
+        print('# Dockerfiles', file=output_file)
+
+        for dockerfile_path, tags in tags_by_dockerfile_path.items():
+            print(f'## {title_of(dockerfile_path)}', file=output_file)
+            for tag in tags:
+                print(f'- `{tag}`', file=output_file)
+            print(file=output_file)
 
 @retry(number_of_attempts=3)
 def build(dockerfile_path: Path,
@@ -40,7 +73,7 @@ def build(dockerfile_path: Path,
         command.append(f'{key}={value}')
 
     command.extend([f'--tag={tag}' for tag in tags])
-    command.append(str(dockerfile_path.parent.resolve()))
+    command.append(str(dockerfile_path.parent))
 
     subprocess.run(command, check=True)
 
@@ -53,25 +86,12 @@ def push(tags: List[str]):
 if __name__ == '__main__':
     build_options = sys.argv[1:]
 
-    with open('config.json', 'r', encoding='utf-8') as input_file:
-        config = json.load(input_file)
+    config = load_config('config.json')
+    generate_readme('README.md', config)
 
     for item in config['builds']:
-        build(dockerfile_path=Path(item['dockerfile']),
-              arguments=item.get('arguments', {}),
+        build(dockerfile_path=item['dockerfile_path'],
+              arguments=item['arguments'],
               tags=item['tags'],
               options=build_options)
         push(tags=item['tags'])
-
-    tags_by_dockerfile = OrderedDict()
-    for item in config['builds']:
-        tags_by_dockerfile.setdefault(item['dockerfile'], []).extend(item['tags'])
-
-    with open('README.md', 'w', encoding='utf-8') as output_file:
-        print('# Dockerfiles', file=output_file)
-
-        for dockerfile, tags in tags_by_dockerfile.items():
-            print(f'## {dockerfile}', file=output_file)
-            for tag in tags:
-                print(f'- `{tag}`', file=output_file)
-            print(file=output_file)
