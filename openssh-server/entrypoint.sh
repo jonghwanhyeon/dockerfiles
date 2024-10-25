7a0b4ff0ce84cc16a0fb0eb0b8cda00fcf1000e6
Jonghwan Hyeon <jonghwanhyeon93@gmail.com>
2024-10-25 13:36:11 +0900

    Remove openssh-server image

--- openssh-server/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -n "$(find /etc/ssh -name 'ssh_host*')" ]; then
-    echo "Regenerating keys..."
-    DEBIAN_FRONTEND=noninteractive dpkg-reconfigure openssh-server
-fi
-
-echo "Starting openssh server..."
-/usr/sbin/sshd -D
